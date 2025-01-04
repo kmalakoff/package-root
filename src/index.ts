9b7f2b06f1f3c0515a85bb392570271c069e2f0d
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-04 04:52:44 -0800

    Upgrade modules

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,14 @@ const existsSync = (test) => {
     return false;
   }
 };
+import type { RootOptions } from './types';
 export * from './types';
-export default function moduleRoot(dir, options) {
+export default function moduleRoot(dir: string, options: RootOptions = {}) {
   let current = dir;
   do {
     const packagePath = path.join(current, 'package.json');
     if (existsSync(packagePath)) {
-      if (!options || options.keyExists === undefined) return current;
+      if (!options.keyExists) return current;
       if (JSON.parse(fs.readFileSync(packagePath, 'utf8'))[options.keyExists] !== undefined) return current;
     }
     const next = path.dirname(current);
